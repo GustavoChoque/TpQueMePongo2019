c50a607b3625b84a7410e35aa0663277f8cce409
EmaSedlar <38228357+EmaSedlar@users.noreply.github.com>
2019-04-24 19:48:39 -0300

    VERIFICAR TELAS
    No se me ocurrio otra forma, esta feo, pero no le encontre solucion.

--- src/main/java/modelo/Prenda.java
+++ src/main/java/modelo/Prenda.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Prenda {
@@ -9,12 +11,13 @@ public class Prenda {
 	public Color colorPrimario;
 	public Color colorSecundario;
 	
+	
 	public Prenda(TipoDePrenda tipo, Tela tel, Color primario) {
 		this.tipo = Objects.requireNonNull(tipo,"Debe especificar la tipo");
-		this.tela = Objects.requireNonNull(tel, "Debe especificar la tela");
+		Objects.requireNonNull(tel, "Debe especificar la tela");
+		this.verificarTela(tipo, tel);
 		this.colorPrimario = Objects.requireNonNull(primario, "El color es obligatorio");
 	}
-	
 	public void setColorSecundario(Color colour) {
 		if(!this.colorPrimario.equals(colour)){
 		this.colorSecundario = colour;
@@ -31,5 +34,26 @@ public class Prenda {
 		
 		return this.tipo.getNombre();
 	}
-	
+	public void verificarTela(TipoDePrenda tipo, Tela telita) {
+		List<Tela> telasPrendasSuperiores = new ArrayList<Tela>();
+		List<Tela> telasPrendasInferiores = new ArrayList<Tela>();
+		List<Tela> telasCalzados = new ArrayList<Tela>();
+		telasCalzados.add(Tela.ALGODON);
+		telasCalzados.add(Tela.CUERO);
+		telasPrendasSuperiores.add(Tela.ALGODON);
+		telasPrendasSuperiores.add(Tela.SEDA);
+		telasPrendasInferiores.add(Tela.ALGODON);
+		telasPrendasInferiores.add(Tela.SEDA);
+		
+		if (tipo.equals(TipoDePrenda.Zapato) && telasCalzados.contains(telita) ) this.tela=telita;
+		if (tipo.equals(TipoDePrenda.Ojotas) && telasCalzados.contains(telita)) this.tela=telita;
+		if (tipo.equals(TipoDePrenda.Camisa) && telasPrendasSuperiores.contains(telita)) this.tela=telita;
+		if (tipo.equals(TipoDePrenda.Remera) &&telasPrendasSuperiores.contains(telita)) this.tela=telita;
+		if (tipo.equals(TipoDePrenda.Pantalon)&& telasPrendasInferiores.contains(telita)) this.tela=telita;
+		if (tipo.equals(TipoDePrenda.Short) &&telasPrendasInferiores.contains(telita)) this.tela=telita;
+		throw new RuntimeException("El tipo de tela no exite para esta prenda");
+		
+		
+
+	}
 }
